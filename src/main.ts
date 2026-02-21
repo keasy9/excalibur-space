b4c595ce3dbeddacc5e87544bc3d599dfa87c172
keasy9 <keasy9@gmail.com>
2026-02-21 21:25:49 +0700

    fix: tsc > vue-tsc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {createApp} from 'vue';
-import Root from '@/vue/Root.vue';
+import Root from './vue/Root.vue';
 import {Config} from '@/game/config';
 
 createApp(Root, { config: Config }).mount('#root');
