57aacf7dbd7d5b55028b7e9f2df427e83795d23d
keasy9 <keasy9@gmail.com>
2026-02-21 21:34:06 +0700

    feat: вытащил канвас из vue, чтобы игра не перезапускалась при HMR vue-компонентов

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import {createApp} from 'vue';
 import Root from './vue/Root.vue';
+import {Engine} from 'excalibur';
 import {Config} from '@/game/config';
+import {Loader} from '@/game/resources';
 
-createApp(Root, { config: Config }).mount('#root');
+createApp(Root).mount('#root');
+
+new Engine({ ...Config, canvasElementId: 'canvas'}).start('main', { loader: Loader })
--- src/vue/Root.vue
+++ src/vue/Root.vue
@@ -1,73 +1,52 @@
 <template>
-    <div class="root">
+    <div
+        class="ui-wrapper"
+        :class="{ 'ui-wrapper--hidden': !uiVisible }"
+    >
         <div
-            class="root__ui-wrapper"
-            :class="{ 'root__ui-wrapper--hidden': !uiVisible }"
+            class="ui-wrapper__toggle"
+            @click="uiVisible = !uiVisible"
         >
-            <div
-                class="root__ui-toggle"
-                @click="uiVisible = !uiVisible"
-            >
-                <span
-                    class="root__ui-toggle-icon"
-                    :class="{ 'root__ui-toggle-icon--mirror': uiVisible }"
-                    v-html="arrowSvg"
-                />
+            <span
+                class="ui-wrapper__toggle-icon"
+                :class="{ 'ui-wrapper__toggle-icon--mirror': uiVisible }"
+                v-html="arrowSvg"
+            />
 
-            </div>
-            <Ui class="root__ui" v-model="State"/>
         </div>
-        <canvas ref="canvas"></canvas>
+        <Ui class="ui-wrapper__ui" v-model="State"/>
     </div>
 </template>
 
 <script setup lang="ts">
-    import {Engine, EngineOptions} from 'excalibur';
-    import {ref, toRaw, useTemplateRef, watch} from 'vue';
-    import {Loader} from '@/game/resources';
+    import {ref} from 'vue';
     import Ui from '@/vue/Ui.vue';
     import arrowSvg from '/assets/icons/arrow.svg?raw';
     import {State} from '@/game/utils/state';
 
-    const props = defineProps<{ config: EngineOptions }>();
-    const canvas = useTemplateRef('canvas');
-
     const uiVisible = ref<boolean>(true);
-
-    let game: Engine|undefined;
-
-    watch(canvas, () => {
-        game?.dispose();
-
-        if (canvas.value) {
-            game = new Engine({ ...toRaw(props.config), canvasElement: canvas.value });
-            game.start('main', { loader: Loader });
-        }
-    });
 
 </script>
 
 <style lang="less">
-    .root {
+    .ui-wrapper {
         --transit: cubic-bezier(.86,0,.07,1) .5s;
 
-        position: relative;
+        pointer-events: none;
+        transition: transform var(--transit);
+        position: absolute;
+        top: 0;
+        left: 0;
+        padding: 8px;
+        background: fade(black, 20);
+        color: lightpink;
 
-        &__ui-wrapper {
-            transition: transform var(--transit);
-            position: absolute;
-            top: 0;
-            left: 0;
-            padding: 8px;
-            background: fade(black, 20);
-            color: lightpink;
-
-            &--hidden {
-                transform: translateX(-100%);
-            }
+        &--hidden {
+            transform: translateX(-100%);
         }
 
-        &__ui-toggle {
+        &__toggle {
+            pointer-events: auto;
             position: absolute;
             top: 0;
             right: -40px;
@@ -78,7 +57,7 @@
             background: fade(black, 20);
         }
 
-        &__ui-toggle-icon {
+        &__toggle-icon {
             width: 100%;
             height: 100%;
             display: block;
@@ -91,7 +70,6 @@
         }
 
         &__ui {
-            pointer-events: none;
 
             & > * {
                 pointer-events: auto;
